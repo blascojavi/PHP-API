8f3bb6a15428d22daad9877fcef119ec179fc8f0
J. Blasco <blascojavi@hotmail.com>
2022-11-12 16:58:51 +0100

    Doctrine

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -1,18 +1,25 @@
 <?php
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 //use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LibraryController extends AbstractController
 {
-/**
+    private $logger;
+
+
+    /**
    * @Route ("/library/list",name="library_list")
 */
 
-public function list(){
+public function list(Request $request,LoggerInterface $logger){
+    $title = $request->get('title','default');
+    $logger->info('list action called 2');
     //$response = new Response();
     $response = new JsonResponse();
     //$response->setContent('<div>Hola Mundo</div>');
@@ -27,7 +34,12 @@ public function list(){
                     'id'=>2,
                     'tittle'=>'El nombre del viento'
 
-                ]
+                ],
+                [
+                    'id'=>3,
+                    'tittle'=>$title
+
+                ],
             ]
         ]);
     return $response;
