05854e7ecc35ccbf2aa3f4041d8b92b7c4b48764
J. Blasco <blascojavi@hotmail.com>
2022-11-12 15:54:09 +0100

    prueba

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -2,19 +2,35 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+//use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LibraryController extends AbstractController
 {
 /**
-   * @Route("/library/list",name="library_list")
+   * @Route ("/library/list",name="library_list")
 */
 
 public function list(){
-    $response = new Response();
-    $response->setContent('<div>Hola Mundo</div>');
+    //$response = new Response();
+    $response = new JsonResponse();
+    //$response->setContent('<div>Hola Mundo</div>');
+    $response->setData([
+        'succes'=>true,
+            'data'=>[
+                [
+                    'id'=>1,
+                    'titte'=>'Hacia rutas salvajes'
+                ],
+                [
+                    'id'=>2,
+                    'tittle'=>'El nombre del viento'
+
+                ]
+            ]
+        ]);
     return $response;
 
 }
-}
+};
